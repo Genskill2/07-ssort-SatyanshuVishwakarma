17988b008cf25b2e342c62d3d7adfaef44feb4d6
SatyanshuVishwakarma <84718007+SatyanshuVishwakarma@users.noreply.github.com>
2021-06-29 08:55:48 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 void swap_max(int arr[],int l,int pos) {
-	if(pos<l-1){
+	if(pos<(l-1)){
 		int max = arr[pos],index=0;
 		for(int i=pos;i<l-1;i++) {
 			if(arr[i]>arr[i+1]){
@@ -26,5 +26,4 @@ void swap_max(int arr[],int l,int pos) {
 void ssort(int arr[],int l) {
 	for(int i=0;i<l;i++)
 		swap_max(arr,9,i);
-
 }
